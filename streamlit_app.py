ca85d11d295992e5e151c6f24054c0d766ef5a5b
Arvind2003 <arvind.e2003@gmail.com>
2025-07-25 18:13:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,8 @@ if name_on_order and ingredients_list:
 
         # Confirmation message
         st.success(f"✅ Your Smoothie is ordered, {name_on_order}!")
+# 🥤 Call the SmoothieFroot API
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+# Check the raw response (status code)
+#st.text(smoothiefroot_response.json())
+sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
